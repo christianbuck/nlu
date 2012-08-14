259750004302648b0bb7d220a9d70aff05c5602c
Nathan Schneider <nschneid@cs.cmu.edu>
2012-08-13 19:29:41 -0700

    timex: Warn about empty time expressions, bug fixes

--- timex.py
+++ timex.py
@@ -27,6 +27,7 @@ Example input, from wsj_0002.0:
 def main(sentenceId, tokens, ww, wTags, depParse, inAMR, alignment, completed):
     amr = inAMR
     new_triples = set()
+    nNewTrip = 0
 
     time_expressions = pipeline.loadTimex(sentenceId)
     for tid, start, end, raw_timex in time_expressions:
@@ -48,7 +49,7 @@ def main(sentenceId, tokens, ww, wTags, depParse, inAMR, alignment, completed):
             x = new_concept(pipeline.token2concept(wd_name), amr)
             new_triples.add((str(mc), 'weekday', str(x)))
 
-        print ('####', t.date_entity)
+        #print('####', t.date_entity)
         for k, v in t.date_entity.iteritems():
             if k=='weekday': continue   # handled above
             if isinstance(v,basestring):
@@ -56,7 +57,7 @@ def main(sentenceId, tokens, ww, wTags, depParse, inAMR, alignment, completed):
                 x = new_concept(v, amr)
                 x = str(x)
             else:   # leave literal numeric values alone
-                print(amr.triples(instances=False))
+                #print(amr.triples(instances=False))
                 x = v
             new_triples.add((str(mc), k, x))
 
@@ -65,42 +66,18 @@ def main(sentenceId, tokens, ww, wTags, depParse, inAMR, alignment, completed):
         for i,j in completed[1]:
             if i >= start and i <= end and j >= start and j <= end:
                 completed[1][(i,j)] = True
+                
+        try:
+            assert len(new_triples)>nNewTrip
+        except AssertionError:
+            print('Warning: Unhandled time expression', file=sys.stderr)
+        nNewTrip = len(new_triples)
 
-
-
-    #for i,j,name,coarse,fine in entities:    # TODO: what is the last one?
-    #    h = choose_head(range(i,j+1), depParse)
-    #    #print((i,j),name,h,depParse[h+1]['dep'], file=sys.stderr)
-    #
-    #    x = alignment[:h] # index of variable associated with i's head, if any
-    #
-    #    if coarse.endswith('_DESC'):
-    #        # make the phrase head word the AMR head concept
-    #        # (could be a multiword term, like Trade Representative)
-    #        if not (x or x==0): # need a new variable
-    #            x = new_concept(pipeline.token2concept(depParse[h][0]['dep']), amr, alignment, h)
-    #            triples.add((str(x), '-DUMMY', ''))
-    #    else:
-    #        if not (x or x==0): # need a new variable
-    #            ne_class = fine.lower().replace('other','') or coarse.lower()
-    #            concept, amr_name = amrify(ne_class, name)
-    #            x = new_concept(pipeline.token2concept(concept)+'-FALLBACK',    # -FALLBACK indicates extra information not in the sentence (NE class)
-    #                            amr, alignment, h)
-    #            n = new_concept('name', amr)
-    #            triples.add((str(x), 'name', str(n)))
-    #            for iw,w in enumerate(amr_name.split()):
-    #                triples.add((str(n), 'op'+str(iw+1), '"'+w+'"'))
-    #
-    #
-    #    for k in range(i,j+1):
-    #        assert not completed[0][k]
-    #        completed[0][k] = True
-    #        #print('completed token',k)
-    #        if k!=h:
-    #            for link in parent_edges(depParse[k]):
-    #                completed[1][link] = True  # we don't need to attach non-head parts of names anywhere else
-    print(list(new_triples))
+    #print(list(new_triples))
+    
     amr = Amr.from_triples(amr.triples(instances=False)+list(new_triples), amr.node_to_concepts)
+    
+    
 
     return depParse, amr, alignment, completed
 
@@ -150,15 +127,25 @@ timex3_ref_to_roles = {
 
 class Timex3Entity(object):
 
-    valid_types = [ 'DATE', 'DATE_RELATIVE', 'DURATION', 'SET' ]
+    valid_types = [ 'DATE', 'DATE_RELATIVE', 'DURATION', 'SET', 'TIME' ]
+
+    # TODO: handle "TIME"
+    '''
+    <TIMEX3 tid="t3" value="TNI" type="TIME">the Night</TIMEX3>
+    <TIMEX3 tid="t3" value="TMO" type="TIME">morning</TIMEX3>
+    <TIMEX3 tid="t3" value="TEV" type="TIME">evening</TIMEX3>
+    <TIMEX3 tid="t3" value="TAF" type="TIME">the afternoon</TIMEX3>
+    <TIMEX3 tid="t3" value="T18:00" type="TIME">6 p.m.</TIMEX3>
+    <TIMEX3 tid="t2" value="XXXX-WXX-4TNI" type="TIME">Thursday night</TIMEX3>
+    '''
 
     def __init__(self, timex, text = None):
         #print timex.attrib
         self.text = timex.text
         self.timex = dict(timex.attrib)
         self.type = self.timex['type']
-        assert self.type in Timex3Entity.valid_types
-        if self.type=='DURATION' and (self.text.endswith(' old') or self.text.endwith('-old')):
+        assert self.type in Timex3Entity.valid_types,self.type
+        if self.type=='DURATION' and (self.text.endswith(' old') or self.text.endswith('-old')):
             self.type = 'AGE'
         
         self.date_entity = {}
